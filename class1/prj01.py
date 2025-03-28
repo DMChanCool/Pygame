0db92b812885b2bb2e8c0037b642b51c1b378fdc
DMChanCool <dmchan008@gmail.com>
2025-03-28 20:31:32 +0800

    feat: 新增 Pygame 初始化及視窗建立程式碼

--- class1/prj01.py
+++ class1/prj01.py
@@ -1,1 +1,18 @@
-ohio
+######################匯入模組######################
+import pygame
+import sys
+
+######################初始化######################
+pygame.init()  # 啟動pygame
+width = 640  # 視窗寬度
+height = 320  # 視窗高度
+
+######################建立視窗及物件######################
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("My Game")
+
+######################循環偵測######################
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # 按x關閉視窗
+            sys.exit()  # 離開
